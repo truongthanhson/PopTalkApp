cbb5b31e58a2b93271da81acb26729c7397c553f
Truong Thanh Son <thanhson.truong@gmail.com>
2017-05-04 23:32:04 +0700

    add data Collections Screen

--- app/src/main/java/com/poptech/poptalk/collections/CollectionsFragment.java
+++ app/src/main/java/com/poptech/poptalk/collections/CollectionsFragment.java
@@ -1,6 +1,9 @@
 package com.poptech.poptalk.collections;
 
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -10,13 +13,25 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.BasePermissionListener;
 import com.poptech.poptalk.Constants;
 import com.poptech.poptalk.R;
 import com.poptech.poptalk.bean.Collection;
+import com.poptech.poptalk.utils.Utils;
 import com.poptech.poptalk.view.ItemDecorationColumns;
 
 import java.util.List;
+import java.util.zip.Inflater;
 
 /**
  * Created by sontt on 26/04/2017.
@@ -47,8 +62,8 @@ public class CollectionsFragment extends Fragment implements CollectionsContract
 
     private void initView() {
         mCollectionsView = (RecyclerView) mView.findViewById(R.id.colletions_list);
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity(), Constants.GRID_COLUMN_COUNT);
-        ItemDecorationColumns itemDecoration = new ItemDecorationColumns(Constants.GRID_COLUMN_COUNT, getResources().getDimensionPixelSize(R.dimen.grid_divider), true);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+        ItemDecorationColumns itemDecoration = new ItemDecorationColumns(2, getResources().getDimensionPixelSize(R.dimen.grid_divider), true);
         mCollectionsView.setHasFixedSize(true);
         mCollectionsView.setLayoutManager(layoutManager);
         mCollectionsView.addItemDecoration(itemDecoration);
@@ -67,6 +82,24 @@ public class CollectionsFragment extends Fragment implements CollectionsContract
     @Override
     public void onResume() {
         super.onResume();
+        Dexter.withActivity(getActivity())
+                .withPermission(Manifest.permission.INTERNET).withListener(new BasePermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                super.onPermissionGranted(response);
+                mPresenter.loadCollections();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                super.onPermissionDenied(response);
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                super.onPermissionRationaleShouldBeShown(permission, token);
+            }
+        }).check();
     }
 
     @Override
@@ -95,6 +128,58 @@ public class CollectionsFragment extends Fragment implements CollectionsContract
 
     @Override
     public void onCollectionsLoaded(List<Collection> collections) {
+        CollectionsAdapter adapter = new CollectionsAdapter(collections,getActivity());
+        mCollectionsView.setAdapter(adapter);
 
     }
+
+    public class CollectionsAdapter extends RecyclerView.Adapter<CollectionViewHolder>{
+        private List<Collection> mCollections;
+        private Context mContext;
+
+        public CollectionsAdapter(List<Collection> collections, Context context) {
+            this.mCollections = collections;
+            this.mContext = context;
+        }
+
+        @Override
+        public CollectionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View rootView = LayoutInflater.from(mContext).inflate(R.layout.item_collection_layout, parent, false);
+            return new CollectionViewHolder(rootView);
+        }
+
+        @Override
+        public void onBindViewHolder(CollectionViewHolder holder, int position) {
+            holder.mDescriptionTv.setText(mCollections.get(position).getDescription());
+            holder.mLanguageTv.setText(mCollections.get(position).getLanguage());
+            Glide.with(mContext)
+                    .load(mCollections.get(position).getThumbPath())
+                    .centerCrop()
+                    .thumbnail(0.5f)
+                    .placeholder(R.color.colorAccent)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(holder.mThumbnailIv);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCollections.size();
+        }
+    }
+
+    public class CollectionViewHolder extends RecyclerView.ViewHolder{
+
+        private View mRootView;
+        private TextView mLanguageTv;
+        private ImageView mThumbnailIv;
+        private TextView mDescriptionTv;
+        public CollectionViewHolder(View itemView) {
+            super(itemView);
+            mRootView = itemView;
+            mDescriptionTv = (TextView)mRootView.findViewById(R.id.tv_description_id);
+            mLanguageTv = (TextView)mRootView.findViewById(R.id.tv_lang_id);
+            mThumbnailIv = (ImageView)mRootView.findViewById(R.id.iv_thumb_id);
+        }
+    }
 }
+
--- app/src/main/java/com/poptech/poptalk/collections/CollectionsModel.java
+++ app/src/main/java/com/poptech/poptalk/collections/CollectionsModel.java
@@ -23,6 +23,27 @@ public class CollectionsModel implements BaseModel {
     }
 
     public List<Collection> getCollections() {
-        return new ArrayList<>();
+        ArrayList<Collection> collections = new ArrayList<>();
+        Collection collection1 = new Collection(1, "collection1","Vietnam","http://kids.nationalgeographic.com/content/dam/kids/photos/Countries/Q-Z/vietnam-ha-long-bay.ngsversion.1412614607489.jpg");
+        Collection collection2 = new Collection(2, "collection2","England","https://images.goaheadtours.com/tour-tile/5176_80_2500_0/a-view-of-big-ben-london-eye-and-the-parliament-in-london-england.jpg");
+        Collection collection3 = new Collection(3, "collection3","Japan","http://japan-magazine.jnto.go.jp/jnto2wm/wp-content/uploads/1608_special_TOTO_main.jpg");
+        Collection collection4 = new Collection(4, "collection4","USA","http://wikitravel.org/upload/shared//thumb/3/37/Space_Needle_Mount_Ranier_Seattle_Washington_USA.jpg/390px-Space_Needle_Mount_Ranier_Seattle_Washington_USA.jpg");
+        Collection collection5 = new Collection(5, "collection5","Germany","https://www.studyabroad.com/sites/default/files/images/Summer-Study-Abroad-Germany-Programs.jpg");
+        Collection collection6 = new Collection(6, "collection6","Australia","http://www.princess.com/images/learn/cruise-destinations/australia-new-zealand-cruises/overview/AU-cruises-640x420.jpg");
+        Collection collection7 = new Collection(7, "collection7","Malaysia","http://apttravel.com/documents/10194/0/port-klang-malaysia.jpg");
+        Collection collection8 = new Collection(8, "collection8","Switzerland","http://www.fodors.com/ee/files/slideshows/11675/switzerland-hero.jpg");
+        Collection collection9 = new Collection(9, "collection9","Sweden","http://s3.amazonaws.com/iexplore_web/images/assets/000/006/463/original/sweden.jpg?1443530953");
+        Collection collection10 = new Collection(10, "collection10","Norway","https://res.cloudinary.com/simpleview/image/upload/c_fill,f_auto,h_280,q_64,w_560/v1/clients/norway/unesco-geirangerfjord-skagefla-waterfall-2-1_6cc6a64a-a204-432e-8753-01ef2080f24e.jpg");
+        collections.add(collection1);
+        collections.add(collection2);
+        collections.add(collection3);
+        collections.add(collection4);
+        collections.add(collection5);
+        collections.add(collection6);
+        collections.add(collection7);
+        collections.add(collection8);
+        collections.add(collection9);
+        collections.add(collection10);
+        return collections;
     }
 }
