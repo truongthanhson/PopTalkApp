7f6115760331541e147e3d65fdd6fcb2c1721df2
Truong Thanh Son <thanhson.truong@gmail.com>
2017-06-16 20:32:22 +0700

    show dialog to choose whether to create speak item from camera or gallery

--- app/src/main/java/com/poptech/poptalk/collections/CollectionsActivity.java
+++ app/src/main/java/com/poptech/poptalk/collections/CollectionsActivity.java
@@ -1,15 +1,24 @@
 package com.poptech.poptalk.collections;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -30,6 +39,7 @@ import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.BaseMultiplePermissionsListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.karumi.dexter.listener.single.BasePermissionListener;
 import com.poptech.poptalk.Constants;
 import com.poptech.poptalk.PopTalkApplication;
@@ -40,6 +50,8 @@ import com.poptech.poptalk.bean.SpeakItem;
 import com.poptech.poptalk.drawer.DrawerMenuAdapter;
 import com.poptech.poptalk.drawer.DrawerMenuDataFactory;
 import com.poptech.poptalk.gallery.GalleryActivity;
+import com.poptech.poptalk.location.LocationListener;
+import com.poptech.poptalk.location.LocationTracker;
 import com.poptech.poptalk.login.LoginActivity;
 import com.poptech.poptalk.login.LoginModel;
 import com.poptech.poptalk.provider.CollectionsModel;
@@ -54,7 +66,12 @@ import com.poptech.poptalk.storyboard.StoryboardActivity;
 import com.poptech.poptalk.storyboard.StoryboardSelecActivity;
 import com.poptech.poptalk.utils.ActivityUtils;
 import com.poptech.poptalk.utils.SaveData;
+import com.poptech.poptalk.utils.StringUtils;
+import com.poptech.poptalk.utils.Utils;
+import com.theartofdev.edmodo.cropper.CropImage;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Random;
 
@@ -64,7 +81,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
  * Created by sontt on 26/04/2017.
  */
 
-public class CollectionsActivity extends AppCompatActivity implements View.OnClickListener, AppMenuOpen, CollectionsFragment.CollectionsFragmentCallback, SpeakItemsFragment.SpeakItemsFragmentCallback {
+public class CollectionsActivity extends AppCompatActivity implements View.OnClickListener, AppMenuOpen, CollectionsFragment.CollectionsFragmentCallback, SpeakItemsFragment.SpeakItemsFragmentCallback, LocationListener {
     private DrawerLayout mDrawerLayout;
 
     private RecyclerView mDrawerMenu;
@@ -78,6 +95,12 @@ public class CollectionsActivity extends AppCompatActivity implements View.OnCli
     private TextView mUserName;
 
     private ImageButton mLogoutButton;
+
+    private LocationTracker mTracker;
+    private Location mLocation = new Location("");
+
+    private String mPhotoString;
+    private String mDateTime = "";
 
     private LoginModel mLoginModel;
     private SpeakItemModel mSpeakItemModel;
@@ -170,6 +193,9 @@ public class CollectionsActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onDestroy() {
+        if (mTracker != null) {
+            mTracker.stopLocation();
+        }
         super.onDestroy();
     }
 
@@ -202,7 +228,47 @@ public class CollectionsActivity extends AppCompatActivity implements View.OnCli
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.fab_add_speak_item:
-                navigateToAddSpeakItem();
+                CharSequence colors[] = new CharSequence[] {"from Camera", "from Gallery"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Create new speak item");
+                builder.setItems(colors, (dialog, which) -> {
+                    if (which == 0) {
+                        Dexter.withActivity(this)
+                                .withPermissions(
+                                        Manifest.permission.CAMERA,
+                                        Manifest.permission.ACCESS_FINE_LOCATION
+                                ).withListener(new MultiplePermissionsListener() {
+                            @Override
+                            public void onPermissionsChecked(MultiplePermissionsReport report) {
+                                if (report.areAllPermissionsGranted()) {
+                                    mTracker = new LocationTracker(CollectionsActivity.this, CollectionsActivity.this);
+                                    if (mTracker.canGetLocation()) {
+                                        runCamera();
+                                    } else {
+                                        // Run camera on activity result
+                                        showSettingsAlert();
+                                    }
+                                } else {
+                                    for (PermissionGrantedResponse permission : report.getGrantedPermissionResponses()) {
+                                        if (permission.getPermissionName().equals(Manifest.permission.CAMERA)) {
+                                            runCamera();
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                            }
+                        }).check();
+                    } else if (which == 1) {
+                        navigateToAddSpeakItem();
+                    }
+                });
+                builder.show();
+
                 break;
             case R.id.user_avatar_id:
                 navigateToChangeAvatar();
@@ -212,6 +278,48 @@ public class CollectionsActivity extends AppCompatActivity implements View.OnCli
                 break;
             default:
                 break;
+        }
+    }
+
+    public void showSettingsAlert() {
+        android.app.AlertDialog.Builder alertDialog = new android.app.AlertDialog.Builder(this);
+        alertDialog.setTitle("Location Setting");
+        alertDialog.setMessage("Location is not enabled. Do you want to enable location?");
+        alertDialog.setPositiveButton("SETTINGS", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(intent, Constants.REQUEST_LOCATION_ENABLE);
+            }
+        });
+        alertDialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                runCamera();
+            }
+        });
+        alertDialog.setCancelable(false);
+        alertDialog.show();
+    }
+
+    private void runCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            Uri mImageCaptureUri;
+            mPhotoString = Environment.getExternalStorageDirectory() + Constants.PATH_APP + "/" + Constants.PATH_PHOTO + "/" + System.currentTimeMillis() + ".jpg";
+            File iFilePath = new File(mPhotoString);
+            try {
+                if (!iFilePath.getParentFile().exists()) {
+                    Utils.forceMkdir(iFilePath.getParentFile());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            mImageCaptureUri = Uri.fromFile(iFilePath);
+            takePictureIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+            takePictureIntent.putExtra("return-data", true);
+            startActivityForResult(takePictureIntent, Constants.REQUEST_AVATAR_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
@@ -270,9 +378,33 @@ public class CollectionsActivity extends AppCompatActivity implements View.OnCli
                 Location location = intent.getExtras().getParcelable(Constants.KEY_GALLERY_LOCATION);
                 onAddSpeakItem(path, date, location);
             }
+        }else if (requestCode == Constants.REQUEST_AVATAR_CAPTURE) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (mTracker != null) {
+                    mLocation = mTracker.getLocation();
+                    mTracker.stopLocation();
+                }
+                mDateTime = getCurrentDate();
+                startActivityCrop(mPhotoString);
+            }
+        } else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(intent);
+            if (resultCode == RESULT_OK) {
+                Intent cropIntent = new Intent();
+//                cropIntent.putExtra(Constants.KEY_GALLERY_PATH, result.getUri().getPath());
+//                cropIntent.putExtra(Constants.KEY_GALLERY_LOCATION, mLocation);
+//                cropIntent.putExtra(Constants.KEY_GALLERY_DATETIME, mDateTime);
+                onAddSpeakItem(result.getUri().getPath(), mDateTime, mLocation);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                Intent errorIntent = new Intent();
+                errorIntent.putExtra("error", error.getMessage());
+                setResult(Activity.RESULT_CANCELED, errorIntent);
+            }
+        } else if (requestCode == Constants.REQUEST_LOCATION_ENABLE) {
+            runCamera();
         }
     }
-
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -431,6 +563,13 @@ public class CollectionsActivity extends AppCompatActivity implements View.OnCli
         openSpeakItemDetailScreen(speakItemId, collectionId);
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        mLocation = location;
+        if (mTracker != null) {
+            mTracker.stopLocation();
+        }
+    }
     public void openSpeakItemDetailScreen(long speakItemId, long collectionId) {
         Intent intent = new Intent(this, SpeakItemDetailActivity.class);
         intent.putExtra(Constants.KEY_SPEAK_ITEM_ID, speakItemId);
@@ -514,5 +653,77 @@ public class CollectionsActivity extends AppCompatActivity implements View.OnCli
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
         finish();
+    }
+
+
+    private void getExifPhoto(String path) {
+        try {
+            ExifInterface exif = new ExifInterface(path);
+            mLocation = new Location("");
+
+            // Parse location
+            String lat = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
+            String lat_ref = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE_REF);
+            String lng = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
+            String lng_ref = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF);
+            if ((!StringUtils.isNullOrEmpty(lat) && !StringUtils.isNullOrEmpty(lat_ref)) &&
+                    (!StringUtils.isNullOrEmpty(lng) && !StringUtils.isNullOrEmpty(lng_ref))) {
+                if (lat_ref.equals("N")) {
+                    mLocation.setLatitude(convertToDegree(lat));
+                } else {
+                    mLocation.setLatitude(0 - convertToDegree(lat));
+                }
+                if (lng_ref.equals("E")) {
+                    mLocation.setLongitude(convertToDegree(lng));
+                } else {
+                    mLocation.setLongitude(0 - convertToDegree(lng));
+                }
+            }
+
+            // Parse date
+            mDateTime = exif.getAttribute(ExifInterface.TAG_DATETIME);
+            SimpleDateFormat inFormat = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
+            SimpleDateFormat outFormat = new SimpleDateFormat("MMM dd, yyyy h:mm a");
+            mDateTime = outFormat.format(inFormat.parse(mDateTime));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Double convertToDegree(String stringDMS) {
+        Double result = null;
+        String[] DMS = stringDMS.split(",", 3);
+
+        String[] stringD = DMS[0].split("/", 2);
+        Double D0 = new Double(stringD[0]);
+        Double D1 = new Double(stringD[1]);
+        Double FloatD = D0 / D1;
+
+        String[] stringM = DMS[1].split("/", 2);
+        Double M0 = new Double(stringM[0]);
+        Double M1 = new Double(stringM[1]);
+        Double FloatM = M0 / M1;
+
+        String[] stringS = DMS[2].split("/", 2);
+        Double S0 = new Double(stringS[0]);
+        Double S1 = new Double(stringS[1]);
+        Double FloatS = S0 / S1;
+
+        result = new Double(FloatD + (FloatM / 60) + (FloatS / 3600));
+
+        return result;
+    }
+
+    private String getCurrentDate() {
+        long date = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy h:mm a");
+        String dateString = sdf.format(date);
+        return dateString;
+    }
+
+    public void startActivityCrop(String path) {
+        CropImage.activity(Uri.fromFile(new File(path)))
+                .start(this);
     }
 }
